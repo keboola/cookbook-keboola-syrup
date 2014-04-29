e685145d836407818bd777d383ff9253c17c3d0c
Martin Halamíček <martin@halamicek.cz>
2014-04-29 16:47:04 +0200

    cloudformation template update

--- recipes/logging.rb
+++ recipes/logging.rb
@@ -3,4 +3,4 @@ include_recipe 'rsyslog'
 include_recipe 'papertrail'
 
 rsyslog_default_template = resources(:template => "/etc/rsyslog.d/50-default.conf")
-rsyslog_default_template.cookbook "syrup"
+rsyslog_default_template.cookbook "keboola-syrup"
