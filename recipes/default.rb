102fd3fef8e25787dd9ac822c5c53d6cb030076f
Martin Halamíček <martin@halamicek.cz>
2014-10-02 16:07:14 +0200

    php recipe fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@
 
 include_recipe "aws"
 include_recipe "keboola-common"
-include_recipe "keboola-connection::php"
+include_recipe "keboola-syrup::php"
 include_recipe "keboola-apache2"
 include_recipe "newrelic::php_agent"
 
