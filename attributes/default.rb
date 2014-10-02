1ef42a9e85ed6491fe110f949c404ee18da80ecb
Martin Halamíček <martin@halamicek.cz>
2014-10-02 16:07:04 +0200

    unused attributes removed

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,6 @@
 default['aws']['aws_access_key_id'] = ''
 default['aws']['aws_secret_access_key'] = ''
 
-default['postfix']['main']['smtp_sasl_password_maps'] = 'static:'
-default['postfix']['main']['transport_maps'] = nil
-
 
 default['php']['packages'] = %w{ php54 php54-devel php-pear php54-pdo php54-mysql php54-mbstring php54-pgsql php54-mcrypt php54-pecl-apc }
 default['php']['directives'] = {
