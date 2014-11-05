fb34404a74e078a23738058cbbc1c4d5c1965209
Martin Halamíček <martin@halamicek.cz>
2014-11-05 16:19:30 +0100

    php55 cookobook

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@
 
 include_recipe "aws"
 include_recipe "keboola-common"
-include_recipe "keboola-php"
+include_recipe "keboola-php55"
 include_recipe "keboola-apache2"
 include_recipe "newrelic::php_agent"
 include_recipe 'sysctl::apply'
