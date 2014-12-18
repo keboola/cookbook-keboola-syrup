7d7c2fef1355413eb542e3f80caa71ef75478d48
Miro Cillik <miro@keboola.com>
2014-12-18 12:11:00 +0000

    feat: freetds.conf for DB extractor

--- recipes/component_ex-db.rb
+++ recipes/component_ex-db.rb
@@ -1,3 +1,12 @@
 
-
+# install mssql php extension
 package "php54-mssql"
+
+# create config for FreeTDS DB driver
+cookbook_file "/etc/freetds.conf" do
+  source "freetds_conf"
+  mode "0644"
+  owner "root"
+  group "root"
+end
+
