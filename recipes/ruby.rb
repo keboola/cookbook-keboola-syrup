6ad64df40aa6ff40278bf5b30e89fe7e443c081f
Martin Halamíček <martin@halamicek.cz>
2014-12-22 15:00:52 +0100

    fix: rvm

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -1,7 +1,10 @@
 
+execute "gpg" do
+	command "gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3"
+end
 
 execute "install rvm" do
- command "curl -sSL https://get.rvm.io | bash -s stable --ruby=2.1.2 --version 1.25.32"
+ command "curl -sSL https://get.rvm.io | bash -s stable --ruby=2.1.2"
 end
 
 execute "rvm use" do
