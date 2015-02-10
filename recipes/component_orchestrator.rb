033c5c72d598d266aa39ec91837da67268ab5a6c
Martin Halamíček <martin@halamicek.cz>
2015-02-10 16:37:09 +0100

    orchestrator - init workers insteadof cron

--- recipes/component_orchestrator.rb
+++ recipes/component_orchestrator.rb
@@ -15,11 +15,9 @@ $i = 1
 $num = node['keboola-syrup']['orchestrator']['workers_count'].to_i
 
 while $i <= $num  do
-   cron "orchestrator worker #{$i}" do
-     user "deploy"
-     minute "*"
-     command "php /www/syrup-router/components/orchestrator/current/vendor/keboola/syrup/app/console orchestrator:queue > /dev/null 2>&1"
-     action "create"
-   end
-   $i +=1
+  execute "start orchestrator worker N=#{$i}" do
+    command "start queue.queue-receive N=#{$i} QUEUE=orchestrator"
+    not_if "status queue.queue-receive N=#{$i} QUEUE=orchestrator"
+  end
+  $i +=1
 end
