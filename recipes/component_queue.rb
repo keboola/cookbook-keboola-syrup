0913d8339a05a87c8d8a1317d6bd8d9982e71942
Martin Halamíček <martin@halamicek.cz>
2015-05-14 07:00:58 +0200

    fix(queue): run kill queue on all servers

--- recipes/component_queue.rb
+++ recipes/component_queue.rb
@@ -24,21 +24,19 @@ end
 
 # start workers
 
+# run kill queue on all servers
+execute "start queue kill queue" do
+  command "start queue.queue-receive-kill"
+  not_if "status queue.queue-receive-kill"
+end
 
 $num = node['keboola-syrup']['queue']['workers_count'].to_i
-
-if $num > 0 then
-  execute "start queue kill queue" do
-    command "start queue.queue-receive-kill"
-    not_if "status queue.queue-receive-kill"
-  end
-end
 
 $i = 1
 while $i <= $num  do
    execute "start queue worker N=#{$i}" do
-	 command "start queue.queue-receive N=#{$i} QUEUE=default"
-	 not_if "status queue.queue-receive N=#{$i} QUEUE=default"
+	    command "start queue.queue-receive N=#{$i} QUEUE=default"
+	    not_if "status queue.queue-receive N=#{$i} QUEUE=default"
    end
    $i +=1
 end
