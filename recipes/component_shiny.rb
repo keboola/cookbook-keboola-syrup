ec082967f9be43dc8ab6e579a303c8e1d6a1aad3
Martin Halamíček <martin@halamicek.cz>
2015-05-15 17:27:05 +0200

    shiny fixes

--- recipes/component_shiny.rb
+++ recipes/component_shiny.rb
@@ -6,9 +6,9 @@ end
 
 
 execute "download shiny server" do
-  command "wget https://s3.amazonaws.com/rstudio-shiny-server-pro-build/centos-5.9/x86_64/shiny-server-commercial-1.3.0.540-rh5-x86_64.rpm > /tmp/shiny.rpm"
+  command "curl https://s3.amazonaws.com/rstudio-shiny-server-pro-build/centos-5.9/x86_64/shiny-server-commercial-1.3.0.540-rh5-x86_64.rpm > /tmp/shiny.rpm"
 end
 
 execute "install shiny server" do
-  command "yum install --nogpgcheck /tmp/shiny.rpm"
+  command "yum -y install --nogpgcheck /tmp/shiny.rpm"
 end
