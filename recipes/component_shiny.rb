8deb543e668fb39469dcef81a677a4784f9622eb
pivnicek <marc@marcs-MacBook-Pro.local>
2015-05-29 12:39:09 +0200

    shiny recipe update

--- recipes/component_shiny.rb
+++ recipes/component_shiny.rb
@@ -1,14 +1,31 @@
+
+# install R
 package "R"
+
+# get Shiny Server rpm
+aws_s3_file "/tmp/shiny-server-commercial-1.3.0.540-rh6-x86_64.rpm" do
+  bucket "keboola-configs"
+  remote_path "syrup/shiny/shiny-server-commercial-1.3.0.540-rh6-x86_64.rpm"
+  aws_access_key_id node[:aws][:aws_access_key_id]
+  aws_secret_access_key node[:aws][:aws_secret_access_key]
+end
+
+# create shiny-server-config
+cookbook_file "/etc/shiny-server/shiny-server.conf" do
+  source "shiny_server_conf"
+  mode "0644"
+  owner "root"
+  group "root"
+end
 
 execute "install shiny R package" do
   command "R -e \"install.packages('shiny', repos='http://cran.rstudio.com/')\""
 end
 
-
-execute "download shiny server" do
-  command "curl https://s3.amazonaws.com/rstudio-shiny-server-pro-build/centos-5.9/x86_64/shiny-server-commercial-1.3.0.540-rh5-x86_64.rpm > /tmp/shiny.rpm"
+execute "install shiny-server" do
+  	command "yum -y --nogpgcheck install /tmp/shiny-server-commercial-1.3.0.540-rh6-x86_64.rpm"
 end
 
-execute "install shiny server" do
-  command "yum -y install --nogpgcheck /tmp/shiny.rpm"
-end
+# execute "create shiny admin user" do
+#  command " echo 'somepassword' | /opt/shiny-server/bin/sspasswd /etc/shiny-server/passwd shiny-admin"
+# end
