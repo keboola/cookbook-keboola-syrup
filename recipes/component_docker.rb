262c87a6e09c7af240da9471f6edfcc1c7854ce5
Ondřej Hlaváček <ondrej.hlavacek@keboola.com>
2015-10-20 14:16:48 +0800

    Update component_docker.rb

--- recipes/component_docker.rb
+++ recipes/component_docker.rb
@@ -54,6 +54,27 @@ execute "docker iptables - 3" do
 end
 
 
+# datadog agent
+execute "install datadog agent" do
+  command "DD_API_KEY=#{node['datadog']['api_key']} bash -c \"$(curl -L https://raw.githubusercontent.com/DataDog/dd-agent/master/packaging/datadog-agent/source/install_agent.sh)\""
+end
+
+execute "modify datadog agent" do
+  command "dd-agent stop"
+  command "usermod -a -G docker dd-agent"
+  command "cp /etc/dd-agent/conf.d/docker_daemon.yaml.example /etc/dd-agent/conf.d/docker_daemon.yaml"
+  command "/etc/init.d/datadog-agent restart"
+end
+
+# docker agent
+execute "pull docker agent" do
+  command "docker pull datadog/docker-dd-agent:latest"
+end
+
+execute "run docker agent" do
+  command "docker run -d --name dd-agent -h `hostname` -v /var/run/docker.sock:/var/run/docker.sock -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e API_KEY=#{node['datadog']['api_key']} datadog/docker-dd-agent:latest"
+end
+
 # init job
 template "/etc/init/queue.queue-receive.conf" do
   source 'queue.queue-receive.conf.erb'
