508b4c98ffa7c0369582ee94d97007823925ef57
Martin Halamíček <martin@halamicek.cz>
2017-03-30 09:55:32 +0200

    fix - datadog agent volumes

--- recipes/docker-monitoring.rb
+++ recipes/docker-monitoring.rb
@@ -1,5 +1,5 @@
 # datadog agent
 
 execute "run docker agent" do
-  command "docker run -d --name dd-agent -e DD_HOSTNAME=`hostname` -v /var/run/docker.sock:/var/run/docker.sock -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e API_KEY=#{node['datadog']['api_key']} -e SD_BACKEND=docker datadog/docker-dd-agent:latest"
+  command "docker run -d --name dd-agent -e DD_HOSTNAME=`hostname` -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /cgroup/:/host/sys/fs/cgroup:ro -e API_KEY=#{node['datadog']['api_key']} -e SD_BACKEND=docker datadog/docker-dd-agent:latest"
 end
