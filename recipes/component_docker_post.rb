66560c51415b7764b657fcbd239e0c7863586a17
Martin Halamíček <martin@halamicek.cz>
2017-08-23 14:19:46 +0200

    docker - garbage collect cron (#30)

--- recipes/component_docker_post.rb
+++ recipes/component_docker_post.rb
@@ -17,4 +17,16 @@ if node['keboola-syrup']['docker']['install_docker'].to_i  > 0
     command "start docker.collect-container-stats N=1"
     not_if "status docker.collect-container-stats N=1"
   end
+
+  cron "docker runner garbage collect" do
+    user "deploy"
+    hour "15"
+    minute "23"
+    command "/www/syrup-router/components/docker/current/vendor/keboola/syrup/app/console docker:garbage-collect >/dev/null 2>&1"
+    action action
+  end
+
 end
+
+
+
