0a5e688d02efe270ab2cfc26696e1ddb58240258
Ondřej Hlaváček <ondrej.hlavacek@keboola.com>
2017-09-19 07:22:11 +0200

    Update default.rb (#32)

--- attributes/default.rb
+++ attributes/default.rb
@@ -130,7 +130,8 @@ default['keboola-syrup']['components'] = [
     },
     {
         id: "restbox",
-        repository_name: "restbox-bundle"
+        repository_name: "restbox-bundle",
+        source: "github"        
     },
     {
         id: "rt-lucky-guess",
