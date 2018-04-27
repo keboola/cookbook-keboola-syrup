52d812f208426cc562922f22eb9092dad8a446e5
Martin Halamíček <martin@halamicek.cz>
2018-04-27 17:48:33 +0200

    docker iptables

--- recipes/component_docker.rb
+++ recipes/component_docker.rb
@@ -70,45 +70,34 @@ if node['keboola-syrup']['docker']['install_docker'].to_i  > 0
     action :start
   end
 
-  execute "allow access to AWS DNS server" do
-    command "iptables -A FORWARD -d 10.0.0.2/32 -o eth0 -j ACCEPT"
-  end
-
-  execute "allow access to Redshift Subnet 1" do
-    command "iptables -A FORWARD -d 10.0.151.0/24 -o eth0 -j ACCEPT"
-  end
-
-  execute "allow access to Redshift Subnet 2" do
-    command "iptables -A FORWARD -d 10.0.150.0/24 -o eth0 -j ACCEPT"
-  end
-
-  execute "allow access to subnet NetHost_VPN_1a" do
-    command "iptables -A FORWARD -d 10.0.222.0/24 -o eth0 -j ACCEPT"
-  end
-
-  execute "allow access to VPN VPC subnet" do
-    command "iptables -A FORWARD -d 10.2.0.0/24 -o eth0 -j ACCEPT"
+  execute "reject connections to instance metadata" do
+    command "iptables --insert FORWARD 1 --in-interface docker+ --destination 169.254.169.254/32 --jump DROP"
   end
 
   execute "reject connections to local subnets" do
-    command "iptables -A FORWARD -d 10.0.0.0/8 -o eth0 -j REJECT --reject-with icmp-port-unreachable"
+    command "iptables --insert FORWARD 1 --in-interface docker+ --destination 10.0.0.0/8 --jump DROP"
   end
 
-  execute "reject connections to instance metadata" do
-    command "iptables -A FORWARD -d 169.254.169.254/32 -o eth0 -j REJECT --reject-with icmp-port-unreachable"
+  execute "allow access to AWS DNS server" do
+    command "iptables --insert FORWARD 1 --in-interface docker+ --destination 10.0.0.2/32 --jump ACCEPT"
   end
 
-  execute "docker iptables - 1" do
-    command "iptables -A FORWARD -o docker0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT"
+  execute "allow access to Redshift Subnet 1" do
+    command "iptables --insert FORWARD 1 --in-interface docker+ --destination 10.0.151.0/24 --jump ACCEPT"
   end
 
-  execute "docker iptables - 2" do
-    command "iptables -A FORWARD -i docker0 ! -o docker0 -j ACCEPT"
+  execute "allow access to Redshift Subnet 2" do
+    command "iptables --insert FORWARD 1 --in-interface docker+ --destination 10.0.150.0/24 --jump ACCEPT"
   end
 
-  execute "docker iptables - 3" do
-    command "iptables -A FORWARD -i docker0 -o docker0 -j ACCEPT"
+  execute "allow access to subnet NetHost_VPN_1a" do
+    command "iptables --insert FORWARD 1 --in-interface docker+ --destination 10.0.222.0/24  --jump ACCEPT"
   end
+
+  execute "allow access to VPN VPC subnet" do
+    command "iptables --insert FORWARD 1 --in-interface docker+ --destination 10.2.0.0/24  --jump ACCEPT"
+  end
+
 
   if node['keboola-syrup']['enable_docker_monitoring'].to_i > 0
     include_recipe "keboola-syrup::docker-monitoring"
